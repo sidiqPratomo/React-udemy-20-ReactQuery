4ec89f4c311a67641147d64212bc834678bdd841
pratomosidiq123 <pratomosidiq123@gmail.com>
2024-08-26 09:44:50 +0700

    react query, useQuery, queryFn, queryKey, signal, useMutation, invalidateQueries, QueryClientProvider

---
+++ src/components/Events/EventDetails.jsx
@@ -0,0 +1,74 @@
+import { Link, Outlet, useParams } from "react-router-dom";
+
+import Header from "../Header.jsx";
+import { useQuery } from "@tanstack/react-query";
+import { fetchEvent } from "../../util/http.js";
+import ErrorBlock from "../UI/ErrorBlock.jsx";
+
+export default function EventDetails() {
+  const params = useParams();
+  const { data, isPending, error, isError } = useQuery({
+    queryKey: ["events", params.id],
+    queryFn: ({ signal }) => fetchEvent({ id: params.id, signal }),
+  });
+
+  let content;
+
+  if (isPending) {
+    content = (
+      <div id="event-details-content" className="center">
+        <p>Fetching event data..</p>
+      </div>
+    );
+  }
+
+  if (isError) {
+    content = (
+      <div id="event-details-content" className="center">
+        <ErrorBlock title="failed to load event" message={error.info?.message || "failed to fetch data please try again later"} />
+      </div>
+    );
+  }
+
+  if (data) {
+    const formattedDate = new Date(data.date).toLocaleDateString('en-US',{
+      day:'numeric',
+      month:'short',
+      year:'numeric'
+    })
+    content = (
+      <>
+        <header>
+          <h1>{data.title}</h1>
+          <nav>
+            <button>Delete</button>
+            <Link to="edit">Edit</Link>
+          </nav>
+        </header>
+        <div id="event-details-content">
+          <img src={`http://localhost:3000/${data.image}`} alt={data.title} />
+          <div id="event-details-info">
+            <div>
+              <p id="event-details-location">{data.location}</p>
+              <time dateTime={`Todo-DateT$Todo-Time`}>
+                {formattedDate} @ {data.time}
+              </time>
+            </div>
+            <p id="event-details-description">{data.description}</p>
+          </div>
+        </div>
+      </>
+    );
+  }
+  return (
+    <>
+      <Outlet />
+      <Header>
+        <Link to="/events" className="nav-item">
+          View all Events
+        </Link>
+      </Header>
+      <article id="event-details">{content}</article>
+    </>
+  );
+}
---
+++ src/components/Events/NewEvent.jsx
@@ -0,0 +1,42 @@
+import { Link, useNavigate } from "react-router-dom";
+
+import Modal from "../UI/Modal.jsx";
+import EventForm from "./EventForm.jsx";
+import { useMutation } from "@tanstack/react-query";
+import { createNewEvent, queryClient } from "../../util/http.js";
+import ErrorBlock from "../UI/ErrorBlock.jsx";
+
+export default function NewEvent() {
+  const navigate = useNavigate();
+
+  const { mutate, isError, error, isPending } = useMutation({
+    mutationFn: createNewEvent,
+    onSuccess:()=>{
+      queryClient.invalidateQueries({queryKey:['events']});
+      navigate('/events')
+    },
+  });
+
+  function handleSubmit(formData) {
+    mutate({ event: formData });
+  }
+
+  return (
+    <Modal onClose={() => navigate("../")}>
+      <EventForm onSubmit={handleSubmit}>
+        {isPending && "Submitting.."}
+        {!isPending && (
+          <>
+            <Link to="../" className="button-text">
+              Cancel
+            </Link>
+            <button type="submit" className="button">
+              Create
+            </button>
+          </>
+        )}
+      </EventForm>
+      {isError && <ErrorBlock title="Failed to create an event" message={error.info?.message || "Failed to create an event please check your input"} />}
+    </Modal>
+  );
+}
