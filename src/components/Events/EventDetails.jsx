85d9f01e0c4a318202d7971f9dc83362bcf6a75e
pratomosidiq123 <pratomosidiq123@gmail.com>
2024-08-26 09:53:04 +0700

    handle delete in event detail

--- src/components/Events/EventDetails.jsx
+++ src/components/Events/EventDetails.jsx
@@ -1,16 +1,32 @@
-import { Link, Outlet, useParams } from "react-router-dom";
+import { Link, Outlet, useNavigate, useParams } from "react-router-dom";
 
 import Header from "../Header.jsx";
-import { useQuery } from "@tanstack/react-query";
-import { fetchEvent } from "../../util/http.js";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { deleteEvent, fetchEvent, queryClient } from "../../util/http.js";
 import ErrorBlock from "../UI/ErrorBlock.jsx";
 
 export default function EventDetails() {
   const params = useParams();
+  const navigate = useNavigate()
   const { data, isPending, error, isError } = useQuery({
     queryKey: ["events", params.id],
     queryFn: ({ signal }) => fetchEvent({ id: params.id, signal }),
   });
+
+  const {mutate} =useMutation({
+    mutationFn:deleteEvent,
+    onSuccess:()=>{
+      queryClient.invalidateQueries({
+        queryKey:['events'],
+        refetchType:'none'
+      })
+      navigate('/events')
+    }
+  })
+
+  function handleDelete(){
+    mutate({id: params.id});
+  }
 
   let content;
 
@@ -41,7 +57,7 @@ export default function EventDetails() {
         <header>
           <h1>{data.title}</h1>
           <nav>
-            <button>Delete</button>
+            <button onClick={handleDelete}>Delete</button>
             <Link to="edit">Edit</Link>
           </nav>
         </header>
