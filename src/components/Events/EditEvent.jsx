52376d9e0390b660801d9a6534e5cb23db7e6d8f
pratomosidiq123 <pratomosidiq123@gmail.com>
2024-08-26 11:11:01 +0700

    implement optimistic updating instead onsuccess

--- src/components/Events/EditEvent.jsx
+++ src/components/Events/EditEvent.jsx
@@ -2,8 +2,8 @@ import { Link, useNavigate, useParams } from "react-router-dom";
 
 import Modal from "../UI/Modal.jsx";
 import EventForm from "./EventForm.jsx";
-import { useQuery } from "@tanstack/react-query";
-import { fetchEvent } from "../../util/http.js";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { fetchEvent, queryClient, updateEvent } from "../../util/http.js";
 import LoadingIndicator from "../UI/LoadingIndicator.jsx";
 import ErrorBlock from "../UI/ErrorBlock.jsx";
 
@@ -16,7 +16,30 @@ export default function EditEvent() {
     queryFn: ({ signal }) => fetchEvent({ id: params.id, signal }),
   });
 
-  function handleSubmit(formData) {}
+  const {mutate} = useMutation({
+    mutationFn: updateEvent,
+    onMutate: async(data)=>{
+      const newEvent = data.event
+
+      await queryClient.cancelQueries({queryKey: ['events', params.id]})
+      const previousEvent = queryClient.getQueryData(["events", params.id]);
+
+      queryClient.setQueryData(["events", params.id], newEvent)
+
+      return {previousEvent}
+    },
+    onError:(error, data, context)=>{
+      queryClient.setQueryData(["events", params.id], context.previousEvent)
+    },
+    onSettled:()=>{
+      queryClient.invalidateQueries(['events', params.id]);
+    }
+  });
+
+  function handleSubmit(formData) {
+    mutate({id: params.id, event: formData})
+    navigate('../');
+  }
 
   function handleClose() {
     navigate("../");
@@ -32,7 +55,7 @@ export default function EditEvent() {
     );
   }
 
-  if (error) {
+  if (isError) {
     content = (
       <>
         <ErrorBlock title="Failed to load event" message={error.info?.message || "Failed to load event please try again later"} />
